62ea773315060df4792556cd547f5b74076f7645
Jason R. Coombs <jaraco@jaraco.com>
2010-08-02 10:10:06 -0400

    Using tag_build in setup.cfg to add dev. Use -b " during release to remove dev tag Bumped to 0.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="paradocx",
-    version="0.3.1",
+    version="0.3.2",
     packages=['paradocx'],
     install_requires=[
         'openpack >= 0.4',

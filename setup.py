dc507769037e48dc6328d7ca4867d8bbb6b45eca
Jason R. Coombs <jaraco@jaraco.com>
2010-08-02 10:07:44 -0400

    Preparing for 0.3.1 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="paradocx",
-    version="0.3.1-dev",
+    version="0.3.1",
     packages=['paradocx'],
     install_requires=[
         'openpack >= 0.4',

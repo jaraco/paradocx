bb05d89fbd3662752bad360a009c08caed42c526
Jason R. Coombs <jaraco@jaraco.com>
2010-08-24 15:09:15 -0400

    Adding readme as long_description

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="paradocx",
     version="0.3.2",
     packages=['paradocx'],
+    long_description=open('README').read(),
     install_requires=[
         'openpack >= 0.4',
     ],

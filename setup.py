888e2b6055457852cec99ae7eeded8c7dcfa4616
Jason R. Coombs <jaraco@jaraco.com>
2011-09-23 13:50:59 -0400

    Made setup script importable

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup
+import os
 
-setup(
+with open(os.path.join(os.path.dirname(__file__), 'README')) as f:
+    long_description = f.read()
+
+setup_params = dict(
     name="paradocx",
     version="0.3.2",
     packages=['paradocx'],
-    long_description=open('README').read(),
+    long_description=long_description,
     install_requires=[
         'openpack >= 0.4',
     ],
 )
 
+if __name__ == '__main__':
+    from setuptools import setup
+    setup(**setup_params)
